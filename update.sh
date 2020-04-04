bc12ba87a697f01bc91862cdd01c4ec94b56e1fb
Mikołaj Pich <m.pich@outlook.com>
2020-04-04 23:33:59 +0200

    Update langs 2020-04-04

--- update.sh
+++ update.sh
@@ -2,67 +2,54 @@
 
 wget \
 	"https://www.tiny.cloud/tinymce-services-azure/1/i18n/download?langs="`
-	`"af_ZA,"`
 	`"ar,"`
-	`"az,"`
 	`"eu,"`
-	`"be,"`
-	`"bn_BD,"`
 	`"bg_BG,"`
 	`"ca,"`
 	`"zh_CN,"`
 	`"zh_TW,"`
 	`"hr,"`
 	`"cs,"`
-	`"cs_CZ,"`
 	`"da,"`
-	`"dv,"`
 	`"nl,"`
-	`"en_CA,"`
-	`"en_GB,"`
-	`"et,"`
+	`"eo,"`
 	`"fi,"`
 	`"fr_FR,"`
 	`"gl,"`
-	`"ka_GE,"`
 	`"de,"`
-	`"de_AT,"`
 	`"el,"`
 	`"he_IL,"`
 	`"hu_HU,"`
 	`"id,"`
-	`"ga,"`
 	`"it,"`
+	`"it_IT,"`
 	`"ja,"`
-	`"kab,"`
 	`"kk,"`
-	`"km_KH,"`
 	`"ko_KR,"`
-	`"lv,"`
 	`"lt,"`
 	`"nb_NO,"`
+	`"fa,"`
 	`"fa_IR,"`
 	`"pl,"`
 	`"pt_BR,"`
 	`"pt_PT,"`
 	`"ro,"`
+	`"ro_RO,"`
 	`"ru,"`
-	`"sr,"`
 	`"sk,"`
+	`"sl,"`
 	`"sl_SI,"`
 	`"es,"`
 	`"es_MX,"`
+	`"es_ES,"`
 	`"sv_SE,"`
 	`"ta,"`
 	`"ta_IN,"`
 	`"th_TH,"`
 	`"tr,"`
 	`"tr_TR,"`
-	`"ug,"`
 	`"uk,"`
-	`"uk_UA,"`
-	`"uz,"`
-	`"vi_VN,"`
+	`"vi,"`
 	`"cy" \
 	-O langs.zip
 
