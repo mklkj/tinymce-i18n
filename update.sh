0c10674a46e9950fdd8185a7639d5a122f83dbf9
Mikołaj Pich <m.pich@outlook.com>
2022-05-02 00:52:55 +0200

    Auto format langs6 files

--- update.sh
+++ update.sh
@@ -5,10 +5,13 @@ wget https://download.tiny.cloud/tinymce/community/languagepacks/6/langs.zip -O 
 wget https://www.tiny.cloud/tinymce-services-azure/1/i18n/download?langs=ar,hy,eu,bn_BD,bg_BG,ca,zh_CN,zh_TW,hr,cs,da,nl,eo,et,fi,fr_FR,gl,de,el,he_IL,hu_HU,id,it,it_IT,ja,kab,kk,ko_KR,ku,lt,nb_NO,fa,fa_IR,pl,pt_BR,pt_PT,ro,ro_RO,ru,ru_RU,sk,sl,sl_SI,es,es_,es_ES,sv_SE,ta,ta_IN,th_TH,tr,tr_TR,uk,vi,cy -O langs5.zip
 wget https://download.tiny.cloud/tinymce/community/languagepacks/4/langs.zip -O langs4.zip
 
+npm install
+
 rm -rf ./langs ./langs5 ./langs6
 
 unzip -q langs6.zip
 mv langs langs6
+./node_modules/.bin/js-beautify -q langs6/*
 
 unzip -q langs5.zip
 mv langs langs5
