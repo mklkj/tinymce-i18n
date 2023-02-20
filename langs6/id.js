dcc0c425ec575ad8ff6de33fe3ea6b27968b541e
mklkj <mklkj@users.noreply.github.com>
2023-02-20 10:19:24 +0000

    Update langs

--- langs6/id.js
+++ langs6/id.js
@@ -1,10 +1,4 @@
-/*!
- * TinyMCE Language Pack
- *
- * Copyright (c) 2022 Ephox Corporation DBA Tiny Technologies, Inc.
- * Licensed under the Tiny commercial license. See https://www.tiny.cloud/legal/
- */
-tinymce.addI18n('id', {
+tinymce.addI18n("id", {
     "Redo": "Ulang",
     "Undo": "Batalkan",
     "Cut": "Potong",
