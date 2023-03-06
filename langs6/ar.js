adc0af40c7d63651c22fbfa4e9524ba89e2035f2
mklkj <mklkj@users.noreply.github.com>
2023-03-06 10:18:38 +0000

    Update langs

--- langs6/ar.js
+++ langs6/ar.js
@@ -1,5 +1,5 @@
 tinymce.addI18n("ar", {
-    "Redo": "\u0625\u0639\u0627\u062f\u0629",
+    "Redo": "\u0625\u0639\u0627\u062f\u0629sdsjhdsd sdkjfh dfjhk sdfjh",
     "Undo": "\u062a\u0631\u0627\u062c\u0639",
     "Cut": "\u0642\u0635",
     "Copy": "\u0646\u0633\u062e",
