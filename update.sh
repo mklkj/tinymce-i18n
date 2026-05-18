1cd5bea5c771c0bddcc282357a4cec4909b0d5e4
Mikołaj Pich <m.pich@outlook.com>
2026-05-18 13:31:16 +0200

    Don't update langs5 in update.sh script due

--- update.sh
+++ update.sh
@@ -5,14 +5,13 @@ set -ex
 wget https://download.tiny.cloud/tinymce/community/languagepacks/8/langs.zip -O langs8.zip
 wget https://download.tiny.cloud/tinymce/community/languagepacks/7/langs.zip -O langs7.zip
 # wget https://download.tiny.cloud/tinymce/community/languagepacks/6/langs.zip -O langs6.zip
-#wget https://download.tiny.cloud/tinymce/community/languagepacks/5/langs.zip -O langs5.zip
-wget https://www.tiny.cloud/tinymce-services-azure/1/i18n/download?langs=ar,az,bg_BG,bn_BD,ca,cs,cy,da,de,dv,el,eo,es,es_,es_ES,es_MX,et,eu,fa,fa_IR,fi,fr_FR,ga,gl,he_IL,hi,hr,hu_HU,hy,id,is_IS,it,it_IT,ja,kab,kk,ko_KR,ku,lt,lv,nb_NO,ne,nl,nl_BE,oc,pl,pt_BR,pt_PT,ro,ro_RO,ru,ru_RU,sk,sl,sl_SI,sq,sr,sv_SE,ta,ta_IN,tg,th_TH,tr,tr_TR,ug,uk,vi,zh-Hans,zh_CN,zh_TW -O langs5.zip
+# wget https://download.tiny.cloud/tinymce/community/languagepacks/5/langs.zip -O langs5.zip
 wget https://download.tiny.cloud/tinymce/community/languagepacks/4/langs.zip -O langs4.zip
 
 npm install
 
 rm -rf ./langs
-rm -rf ./langs5
+# rm -rf ./langs5
 # rm -rf ./langs6
 rm -rf ./langs7
 rm -rf ./langs8
@@ -71,8 +70,8 @@ done
 # ./node_modules/.bin/js-beautify -q langs6/*
 
 # langs5
-unzip -q langs5.zip
-mv langs langs5
+# unzip -q langs5.zip
+# mv langs langs5
 
 # langs4
 unzip -q langs4.zip
